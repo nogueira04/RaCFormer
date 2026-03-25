2cb77d280f2cd6db29b9687d772b76280b9123d2
Gabriel <gnmp@cin.ufpe.br>
2026-03-25 05:27:52 +0000

    Revert "experiment(007): zero velocity for static classes (barrier, traffic_cone)"
    This reverts commit a65daa932bec385d4969d3b37ea7706f5a391cc9.

--- configs/racformer_mini_exp007_static_vel.py
+++
@@ -1,4 +0,0 @@
-_base_ = ['./racformer_r50_nuimg_704x256_f8.py']
-data = dict(val=dict(max_samples=300))
-# barrier=5, traffic_cone=9
-model = dict(pts_bbox_head=dict(bbox_coder=dict(static_classes=[5, 9])))
